444f1bfc9a3635ee1c84b5397190b62d4ae04d28
Ezequiel <ezegom01@gmail.com>
2026-03-03 14:44:38 -0600

    Prueba Controladores

--- app/Http/Controllers/CalificacionesController.php
+++ app/Http/Controllers/CalificacionesController.php
@@ -6,7 +6,15 @@ use Illuminate\Http\Request;
 
 class CalificacionesController extends Controller
 {
-    public function verCalificaciones($matricula) {
+    public function verCalificaciones($matricula) 
+    {
         return 'Calificaciones de ' . $matricula;
+    }
+
+    public function mayorEdad($edad)
+    {
+        $mensaje = 'Tienes ' . $edad . ' años. ';
+
+        return (($edad < 18) ? $mensaje . ' Y eres menor de edad' : $mensaje . ' Y eres mayor de edad');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomeController::class);
 
 Route::get('/post', [PostController::class, 'index']);
-Route::get('/calificaciones', [CalificacionesController::class, 'verCalificaciones']);
-
-Route::get('/edad/{edad}', function($edad) { 
-    $mensaje = 'Tienes ' . $edad;
-
-    return (($edad >= 18) ? $mensaje . ' y eres mayor de edad' : $mensaje . ' y eres menor de edad');
-    
-});
+Route::get('/calificaciones/{matricula}', [CalificacionesController::class, 'verCalificaciones']);
+Route::get('/edad/{edad}', [CalificacionesController::class, 'mayorEdad']);
